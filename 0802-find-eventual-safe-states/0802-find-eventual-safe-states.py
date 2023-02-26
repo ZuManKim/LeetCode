44271a4d69366cf34235b4bb52abfbec3a92fc10
ZuManKim <47964552+ZuManKim@users.noreply.github.com>
2023-02-26 07:30:14 -0800

    Time: 768 ms (28.05%), Space: 25 MB (8.01%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,22 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        safe_nodes = [False] * len(graph)
+        outbound = [set(out) for out in graph]
+        inbound = [set() for _ in graph]
+        queue = collections.deque()
+        
+        for i, outgoing in enumerate(outbound):
+            if not outgoing:
+                queue.append(i)
+            for j in outgoing:
+                inbound[j].add(i)
+                
+        while queue:
+            j = queue.popleft()
+            safe_nodes[j] = True
+            for i in inbound[j]:
+                outbound[i].remove(j)
+                if len(outbound[i]) == 0:
+                    queue.append(i)
+
+        return [i for i, is_safe in enumerate(safe_nodes) if is_safe]
