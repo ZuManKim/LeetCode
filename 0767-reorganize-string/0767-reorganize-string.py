1a82422cf8b743a13173526adbaff08b26a3e431
ZuManKim <47964552+ZuManKim@users.noreply.github.com>
2023-10-08 13:36:33 +0900

    Time: 43 ms (46.72%), Space: 16.2 MB (74.65%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,27 +1,20 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         
-        char_counts = Counter(s)
-        letter, max_cnt = char_counts.most_common(1)[0]
-       
-        if max_cnt > (len(s)+1)//2:
-            return ""
+        res = []
+        pq = [(-count, char) for char, count in Counter(s).items()]
+        heapq.heapify(pq)
         
-        ans = [''] * len(s)
-        idx = 0
+        prev_count, prev_char = heapq.heappop(pq)
+        res += [prev_char]
         
-        while char_counts[letter] != 0:
-            ans[idx] = letter
-            idx += 2
-            char_counts[letter] -= 1
+        while pq:
+            cur_count, cur_char = heapq.heappop(pq)
+            res += [cur_char]
+            prev_count += 1
+            if prev_count < 0:
+                heapq.heappush(pq, (prev_count, prev_char))
+
+            prev_count, prev_char = cur_count, cur_char
         
-        for char, cnt in char_counts.items():
-            while cnt > 0:
-                if idx >= len(s):
-                    idx = 1
-                
-                ans[idx] = char
-                idx += 2
-                cnt -= 1
-                
-        return "".join(ans)
+        return ''.join(res) if len(res) == len(s) else ''
