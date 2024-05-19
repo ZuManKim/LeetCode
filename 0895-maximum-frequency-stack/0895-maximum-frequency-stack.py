42c8fa862a28c454a7cd59eab2fd72e4972381cc
ZuManKim <47964552+ZuManKim@users.noreply.github.com>
2024-05-19 21:11:53 +0900

    Time: 249 ms (40.35%), Space: 25.5 MB (37.26%) - LeetHub

--- 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
@@ -1,24 +1,24 @@
 class FreqStack:
 
     def __init__(self):
-        self.freq = collections.Counter()
-        self.freq_group = collections.defaultdict(list)
+        self.freq = Counter()
+        self.freq_stack = defaultdict(list)
         self.max_freq = 0
 
     def push(self, val: int) -> None:
-        freq, freq_group = self.freq, self.freq_group
-        freq[val] += 1 
-        self.max_freq = max(self.max_freq, freq[val])
-        freq_group[freq[val]].append(val)
+        freq, freq_stack = self.freq, self.freq_stack
+        freq[val] += 1
+        freq_stack[freq[val]].append(val)
+        self.max_freq = max(self.max_freq, freq[val])   
 
     def pop(self) -> int:
-        freq, freq_group, max_freq  = self.freq, self.freq_group, self.max_freq
-        res = freq_group[max_freq].pop()
-        if not freq_group[max_freq]:
+        freq, freq_stack, max_freq = self.freq, self.freq_stack, self.max_freq
+        x = freq_stack[max_freq].pop()
+        if not freq_stack[max_freq]:
             self.max_freq -= 1
-        freq[res] -= 1
-        
-        return res
+        freq[x] -= 1
+        return x
+
 
 # Your FreqStack object will be instantiated and called as such:
 # obj = FreqStack()
