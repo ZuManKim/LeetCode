5f1207f2ea12d397a1b4b12a39e28e8f8f55b5f1
ZuManKim <47964552+ZuManKim@users.noreply.github.com>
2024-06-02 22:07:49 +0900

    Time: 237 ms (98.75%), Space: 17.7 MB (84.59%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,29 +1,17 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        
-        paths = [[-1] * n for _ in range(m)]
-        
-        def dfs(i, j, prev):
-            
-            if i < 0 or j < 0 or i >= m or j >= n or matrix[i][j] <= prev:
-                return 0
-            
-            if paths[i][j] != -1:
-                return paths[i][j]
-            
-            cur = matrix[i][j]
-            left = dfs(i-1, j, cur)
-            right = dfs(i+1, j, cur)
-            top = dfs(i, j-1, cur)
-            bottom = dfs(i, j+1, cur)
-            
-            paths[i][j] = max(left, right, top, bottom) + 1
-            
-            return paths[i][j]
-        
-        for i in range(m):
-            for j in range(n):
-                dfs(i, j, -1)
-        
-        return max(map(max, paths))
+        dp = [[0] * n for _ in range(m)]
+
+        def dfs(i, j):
+            if not dp[i][j]:
+                val = matrix[i][j]
+                dp[i][j] = 1 + max(
+                    dfs(i-1, j) if i and matrix[i-1][j] > val else 0,
+                    dfs(i, j-1) if j and matrix[i][j-1] > val else 0,
+                    dfs(i+1, j) if i < m-1 and matrix[i+1][j] > val else 0,
+                    dfs(i, j+1) if j < n-1 and matrix[i][j+1] > val else 0
+                )
+            return dp[i][j]
+    
+        return max(dfs(i, j) for i in range(m) for j in range(n))
